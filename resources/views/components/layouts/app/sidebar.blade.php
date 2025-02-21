1b3c2835ff24681f9395e497787037ceecb8b7fa
Taylor Otwell <taylor@laravel.com>
2025-02-21 13:25:28 -0600

    remove unnecessary code

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -35,17 +35,7 @@
                     :name="auth()->user()->name"
                     :initials="auth()->user()->initials()"
                     icon-trailing="chevrons-up-down"
-                >
-                    <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                        <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                            <span
-                                class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
-                            >
-                                {{ auth()->user()->initials() }}
-                            </span>
-                        </span>
-                    </div>
-                </flux:profile>
+                />
 
                 <flux:menu class="w-[220px]">
                     <flux:menu.radio.group>

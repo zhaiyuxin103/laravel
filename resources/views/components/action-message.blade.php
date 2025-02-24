455aebfd62e999a55341e70426412383f2144676
Syofyan Zuhad 🇵🇸 <sofyanzuhad2@gmail.com>
2025-02-25 03:28:56 +0700

    Update text color on action-message.blade.php (#17)

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -8,7 +8,7 @@
     x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms
     style="display: none"
-    {{ $attributes->merge(['class' => 'text-sm text-gray-600']) }}
+    {{ $attributes->merge(['class' => 'text-sm']) }}
 >
     {{ $slot->isEmpty() ? __('Saved.') : $slot }}
 </div>

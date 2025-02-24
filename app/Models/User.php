3848c9b56c664b5c0a1928e24669c6bf78905cdc
Chris Teyerl <44904418+christeyerl@users.noreply.github.com>
2025-02-24 20:39:17 +0100

    Sync with laravel/laravel (#14)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable // implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
